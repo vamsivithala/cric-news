214ea29338a5e00920463eccbfaf04df3fe466dc
vamsivithala <saivamsivithala.com>
2023-03-22 14:21:16 +0530

    app routes updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'players',
+  loadChildren: () => import('./players/players.module').then(m => m.PlayersModule),
 
+}, {
+  path: 'teams',
+  loadChildren: () => import('./teams/teams.module').then(m => m.TeamsModule)
+},
+{
+  path: 'matches',
+  loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule)
+}];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
